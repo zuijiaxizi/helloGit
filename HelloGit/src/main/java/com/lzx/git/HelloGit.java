0ac17c083616b5524126d6daf7ae0d0c9fd8208f
zuijiaxizi <2739638173@qq.com>
2020-08-22 16:09:31 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/lzx/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.lzx.git;
+
+/**
+ * @author ezio
+ * @date 2020/8/22 16:02
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
